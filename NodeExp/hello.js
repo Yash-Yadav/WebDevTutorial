b1491ca02e15278898b55079236d8edb3dc0313f
Yash-Yadav <yash-yadav@outlook.com>
2019-07-05 21:44:47 +0530

    Commit

---
+++ NodeExp/hello.js
@@ -0,0 +1,14 @@
+for(var i=0; i<5; i++){
+  console.log("Hello"+i);
+}
+
+echo("Echo!!!", 10);
+echo("Hey!!!", 3);
+
+function echo(str, num)
+{
+  for (let i = 0; i <= num; i++)
+  {
+    console.log(str);
+  }
+}
