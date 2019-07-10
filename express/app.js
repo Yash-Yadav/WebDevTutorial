cd66b6d0df10851f2457e429d24e1549ed0812bc
Yash-Yadav <yash-yadav@outlook.com>
2019-07-10 22:47:25 +0530

    commit

--- express/app.js
+++ express/app.js
@@ -4,19 +4,38 @@ var app = express();
 //"/" => "Hi There"
 app.get("/", function(req, res) {
   res.send("Hi There!");
-})
+});
 
 //"/bye" => "GoodBye"
 app.get("/bye", function(req, res) {
   res.send("GoodBye!");
-})
+});
 
 //"/dog" => "MEOW!"
 app.get("/dog", function(req, res) {
   res.send("MEOW!");
   console.log("Someone made a /dog request");
-  
-})
+});
+
+app.get("/r/:subredditName", function(req,res) {
+  var subreddit = req.params.subredditName;
+  console.log(subreddit);
+  res.send("Welcome " + subreddit.toUpperCase()
+    + " SUBREDDIT");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req,res) {
+  console.log(req.params);  
+  res.send("Welcome to the Comments Page!");
+});
+
+//Handling invalid Get Request
+// Remember the Order of Call backs Matters
+// So the route will be first->
+//hi-> bye-> dog-> then something Gibrish...
+app.get("*", function(req, res) {
+  res.send("You are a STAR!!!");
+});
 
 app.listen(3000, function () {
   console.log("Server has started");
