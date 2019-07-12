812599eb53c368877221ff23ce3d16a665e1ca69
Yash-Yadav <yash-yadav@outlook.com>
2019-07-12 21:58:10 +0530

    Commit

--- express/ejs-exp/app.js
+++ express/ejs-exp/app.js
@@ -1,13 +1,26 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-  res.render("home.ejs");
+  res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res){
   var thing = req.params.thing;
-  res.render("love.ejs", {thingVar: thing});
+  res.render("love", {thingVar: thing});
+});
+
+app.get("/posts", function(req, res){
+  var posts = [
+    {title: "Post 1", author: "Yash"},
+    {title: "My Adorable pet", author: "DustiBun"},
+    {title: "Post 3", author: "SussiePoo"}
+  ];
+
+  res.render("posts", {posts: posts});
 });
 
 app.listen(3000, function () {
