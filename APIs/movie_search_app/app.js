8c8fdc331611ad6813e503b82b1f53e15e937dee
Yash-Yadav <yash-yadav@outlook.com>
2019-07-16 01:37:06 +0530

    Commit

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  res.render("Search");
+});
 
 app.get("/results", function(req,res) {
-  res.send("Hello!");
+  request("http://www.omdbapi.com/?s=Star&apikey=d90ccb36", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      res.render("results", {data: data});
+    }
+  });
 });
 
 app.listen(3000, function() {
