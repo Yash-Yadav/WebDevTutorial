db31bde1943dad45ea872ad9cf5bfca5aa1d4406
Yash-Yadav <yash-yadav@outlook.com>
2019-07-16 17:52:46 +0530

    Commit

--- Databases/cats.js
+++ Databases/cats.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/cat_app");
+mongoose.connect("mongodb://localhost/cat_app", {useNewUrlParser: true});
 
 var catSchema = new mongoose.Schema({
   name: String,
@@ -12,20 +12,45 @@ var Cat = mongoose.model("Cat", catSchema);
 
 // adding a new cat to the DB
 
-var george = new Cat({
-  name: "George",
-  age: 11,
-  temperament: "Grouchy"
-});
+// var george = new Cat({
+//   name: "Mrs. Norris",
+//   age: 7,
+//   temperament: "Evil"
+// });
 
-george.save(function(error, cat) {
-  if(err) {
-    console.log("Something went wrong!");
-  }
-  else {
-    console.log("We just saved a Cat to the DB");
-    console.log(cat);
-  }
-});
+// george.save(function(err, cat) {
+//   if(err) {
+//     console.log("Something went wrong!");
+//   }
+//   else {
+//     console.log("We just saved a Cat to the DB");
+//     console.log(cat);
+//   }
+// });
+
+// // Creating a new Cat
+// Cat.create({
+//   name: "Snow White",
+//   age: 15,
+//   temperament: "Bland"
+// }, function(err, cat) {
+//   if(err) {
+//     console.log("Something went wrong!");
+//     console.log(err);
+//   }
+//   else {
+//     console.log(cat);
+//   }
+// });
 
 // retrieve all cats from the db and Console.Log oneach one
+Cat.find({}, function(err, cats) {
+  if(err) {
+    console.log("Something went wrong!");
+    console.log(err);
+  }
+  else {
+    console.log("All the Cats");
+    console.log(cats);
+  }
+})
