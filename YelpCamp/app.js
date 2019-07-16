5d1c88396e75055b026d64ec16ff3d87dd3a9ab7
Yash-Yadav <yash-yadav@outlook.com>
2019-07-16 15:09:55 +0530

    Commit

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,36 +4,39 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+  {name: "Site1", image: "https://pixabay.com/get/52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c732c79d69544c650_340.jpg"},
+  {name: "Site2", image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732c79d69544c650_340.jpg"},
+  {name: "Site3", image: "https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732c79d69544c650_340.jpg"},
+  {name: "Site4", image: "https://pixabay.com/get/57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c732c79d69544c650_340.jpg"}
+];
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-  var campgrounds = [
-    {name: "Site1", image: "https://pixabay.com/get/52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c732c79d69544c650_340.jpg"},
-    {name: "Site2", image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732c79d69544c650_340.jpg"},
-    {name: "Site3", image: "https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732c79d69544c650_340.jpg"},
-    {name: "Site4", image: "https://pixabay.com/get/57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c732c79d69544c650_340.jpg"}
-  ];
 
   res.render("campgrounds", {campgrounds: campgrounds});
 });
 
-
 app.post("/campgrounds", function(req, res) {
-  res.send("POST Working Fine...")
   //Get data from Form and add to Campgrounds Array
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
   //Redirect to the Campgrounds Page
+  res.redirect("/campgrounds");
 });
-
 
 app.get("/campgrounds/new", function(req, res) {
   res.render("new");
 });
 
 
-
 app.listen(3000, function() {
   console.log("The YelpCamp Server has Started");
 });
+
+// Image Source: https://photosforclass.com/search/camping
