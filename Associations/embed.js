f6ccfc25441949228f29f83986fff9896e8926c2
Yash-Yadav <yash-yadav@outlook.com>
2019-07-18 22:09:35 +0530

    Commit

--- Associations/embed.js
+++ Associations/embed.js
@@ -1,29 +1,55 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo", {useNewUrlParser: true});
 
-// USER - email, name
-var userSchema = new mongoose.Schema({
-  email: String,
-  name: String
-});
-var User = mongoose.model("User", userSchema);
-
 // POST - title, content
 var postSchema = new mongoose.Schema({
   title: String,
-  contnet: String
+  content: String
 });
-var User = mongoose.model("Post", postSchema);
 
-var newUser = new User({
-  email: "charlie@brown.edu",
-  name: "Charlie Brown"
+// USER - email, name
+var userSchema = new mongoose.Schema({
+  email: String,
+  name: String,
+  posts: [postSchema]
 });
-newUser.save(function(err, user) {
+var User = mongoose.model("User", userSchema);
+
+var Post = mongoose.model("Post", postSchema);
+
+// var newUser = new User({
+//   email: "hermione@hogwarts.edu",
+//   name: "Hermione Granger"
+// });
+// newUser.posts.push({
+//   title: "How to brew polyjuice potion",
+//   content: "Just kidding, go to potion class & learn it!"
+// });
+
+// newUser.save(function(err, user) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(user);
+//   }
+// });
+
+// Finding an Existing Doc
+User.findOne({name: "Hermione Granger"}, function(err, user) {
   if (err) {
     console.log(err);
   } else {
     console.log(user);
-    
+    user.posts.push({
+      title: "3 Things I really hate",
+      content: "Voldemort. Voldemort. Voldemort."
+    });
+    user.save(function(err, user) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(user);
+      }
+    });
   }
-})
+});
