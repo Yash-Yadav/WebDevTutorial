5c0e2bddfe6f20a3c20db4aec66777bf7a66adf9
Yash-Yadav <yash-yadav@outlook.com>
2019-07-18 22:18:27 +0530

    Commit

--- Associations/references.js
+++ Associations/references.js
@@ -1,54 +1,39 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo_2", {useNewUrlParser: true});
 
-// POST - title, content
-var postSchema = new mongoose.Schema({
-  title: String,
-  content: String
-});
-var Post = mongoose.model("Post", postSchema);
+var Post = require("./models/post");
+var User = require("./models/user");
 
-// USER - email, name
-var userSchema = new mongoose.Schema({
-  email: String,
-  name: String,
-  posts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Post"
+
+// // Finding the Doc
+// User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(user);
+//   }
+// })
+
+//Posting the Posts
+Post.create({
+  title: "How to cook the best burger pt 4",
+  content: "hohohohohohoHOHOHOHOHOhohohoho..."
+}, function(err, post) {
+  User.findOne({email: "bob@gmail.com"}, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      foundUser.posts.push(post);
+      foundUser.save(function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(data);
+        }
+      });
     }
-  ]
+  })
 });
-var User = mongoose.model("User", userSchema);
-
-User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(user);
-  }
-})
-
-// //Posting the Posts
-// Post.create({
-//   title: "How to cook the best burger pt 3",
-//   content: "hehehehehehhehehehe..."
-// }, function(err, post) {
-//   User.findOne({email: "bob@gmail.com"}, function(err, foundUser) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       foundUser.posts.push(post);
-//       foundUser.save(function(err, data) {
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           console.log(data);
-//         }
-//       });
-//     }
-//   })
-// });
 
 // User.create({
 //   email: "bob@gmail.com",
