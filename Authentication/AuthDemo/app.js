c85ac6938a4ab9ba646e9eaedf811ac81ba61cc8
Yash-Yadav <yash-yadav@outlook.com>
2019-07-26 23:48:51 +0530

    Commit

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,4 +1,34 @@
-var express = require("express");
+var express              = require("express"),
+   mongoose              = require("mongoose"),
+   passport              = require("passport"),
+   bodyParser            = require("body-parser"),
+   User            = require("./models/user"),
+   LocalStrategy         = require("passport-local"),
+   passportLocalMongoose = require("passport-local-mongoose");
+mongoose.connect("mongodb://localhost/auth_demo_app", {useNewUrlParser: true});
 
 var app = express();
 app.set('view engine', 'ejs');
+
+app.use(require("express-session")({
+  secret: "Sandy is the best cutest Dog in this World!"
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser);
+passport.deserializeUser(User.deserializeUser);
+
+app.get("/", function(req,res) {
+  res.render("home");
+})
+
+app.get("/secret", function(req,res) {
+  res.render("secret");
+})
+
+
+app.listen(3000, function() {
+  console.log("Server has Started");
+})
