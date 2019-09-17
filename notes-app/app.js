2ee7f6be99af6763df9999505ff52161bfb3d100
Yash-Yadav <yash-yadav@outlook.com>
2019-09-17 23:52:33 +0530

    Command Line Args

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,5 +2,18 @@ const validator = require('validator')
 const getNotes = require('./notes')
 const chalk = require('chalk')
 
-const msg = getNotes()
-console.log(chalk.bold.white.bgGreen(msg))
+// const msg = getNotes()
+// console.log(chalk.bold.white.bgGreen(msg))
+
+const command = process.argv[2]
+
+if (command === 'add') {
+  console.log('Adding Note...')
+}
+else if (command === 'remove') {
+  console.log('Removing Note...')
+}
+else {
+  console.log('Invalid Command')
+}
+console.log(process.argv)
