448d9ac7f258faf29a0e7aca18d91243328a1e9a
Yash-Yadav <yash-yadav@outlook.com>
2019-09-17 21:40:15 +0530

    Commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,10 @@
-const name = 'Yash'
-console.log(name);
+const getNotes = require('./notes')
+
+const msg = getNotes()
+
+console.log(msg)
+
+
+// var add = require('./utils.js')
+// const sum = add(4, 12)
+// console.log(sum)
