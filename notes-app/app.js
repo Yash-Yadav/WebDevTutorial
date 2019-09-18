51bebc46aae37258673ca1ce8723b2177b1d759c
Yash-Yadav <yash-yadav@outlook.com>
2019-09-18 17:53:01 +0530

    using yargs pkg

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,27 @@
-const validator = require('validator')
-const getNotes = require('./notes')
+// const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes')
 
-// const msg = getNotes()
-// console.log(chalk.bold.white.bgGreen(msg))
+console.log(yargs.argv)
 
-const command = process.argv[2]
 
-if (command === 'add') {
-  console.log('Adding Note...')
-}
-else if (command === 'remove') {
-  console.log('Removing Note...')
-}
-else {
-  console.log('Invalid Command')
-}
-console.log(process.argv)
+// const validator = require('validator')
+// const getNotes = require('./notes')
+// const chalk = require('chalk')
+
+// // const msg = getNotes()
+// // console.log(chalk.bold.white.bgGreen(msg))
+
+// const command = process.argv[2]
+
+// if (command === 'add') {
+//   console.log('Adding Note...')
+// }
+// else if (command === 'remove') {
+//   console.log('Removing Note...')
+// }
+// else {
+//   console.log('Invalid Command')
+// }
+// console.log(process.argv)
